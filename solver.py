810733d1d95c521adeeb776576b63e55ff1af945
ZoeAliki <zabreedveld@gmail.com>
2025-12-11 14:28:33 +0100

    Update notebooks and solver

--- solver.py
+++ solver.py
@@ -59,3 +59,57 @@ def setup_swe_problem(Lx, Ly, Nx, Ny, U_inflow):
     plt.show()
 
     return mesh, W, w, u, eta, v, q, inflow, outflow, walls
+
+#for safety, may be necessARY LAter
+#H_safe = conditional(gt(H, 1e-3), H, 1e-3)
+
+def solve_tidal_flow_velocities(turbine_positions, w, W, mesh, bcs, rho, depth, nu, cb, g, C_T, A_T, sigma):
+# Trial and test functions 
+    (u_, eta_) = split(w)      # trial: velocity, free-surface
+    (v_, q_) = TestFunctions(W)
+
+    n = FacetNormal(mesh)
+    H = depth + eta_     # for full nonlinear free-surface coupling
+    f_u = Constant((0, 0)) #no internal forcing term - coriolis/windstress/pressure gradient - do look into tidal driving
+
+# --- Turbine-induced momentum sink coefficient field ----------------------
+    x, y = SpatialCoordinate(mesh)
+
+# Build Gaussian field for all turbines
+
+    Ct_field = 0
+    for (x_i, y_i) in turbine_positions:
+        Ct_field += 0.5 * C_T * A_T / (2.0 * np.pi * sigma**2) * exp(-((x - x_i)**2 + (y - y_i)**2) / (2.0 * sigma**2))
+
+# Define the full nonlinear residual form F
+    F = (inner(nu * grad(u_), grad(v_)) * dx #viscosity
+         + inner(dot(u_, nabla_grad(u_)), v_) * dx #advection
+         - g * div(H * v_) * eta_ * dx
+     #- g * H * eta_ * div(v_) * dx #pressure gradient for incompr LOOK AT THIS LATER, TWICE THE ELEVATION TAKEN INTO ACCOUNT??
+         + (cb/H) * inner(u_ * sqrt(dot(u_, u_)), v_) * dx) #bottom friction
+
+# Turbine momentum sink using spatially varying field
+    F += (Ct_field / H) * inner(u_ * sqrt(dot(u_, u_)), v_) * dx
+    F += H * div(u_) * q_ * dx - inner(f_u, v_) * dx  # Full residual (no separate L) mass conservation/continuity
+
+# Solve nonlinear problem with Newton's method
+    solve(F == 0, w, bcs,
+          solver_parameters={"newton_solver": {
+              "linear_solver": "mumps",  # or "petsc" with good preconditioner
+              "absolute_tolerance": 1e-8,
+              "relative_tolerance": 1e-7,
+              "maximum_iterations": 30,
+              "relaxation_parameter": 1.0
+          }})
+
+#compute power 
+    velocity = w.sub(0, deepcopy=True)
+    turbine_powers, _ = compute_turbine_power(velocity, turbine_positions, rho, C_T, A_T)
+    total_power = np.sum(turbine_powers)
+    print(f"The total power is {total_power/1e3:.1f} kW")
+
+    return total_power, velocity
+
+#def objective_function1(turbine_positions, velocity_function, rho, C_T, A_T):
+#    turbine_power, velocity = solve_tidal_flow_velocities(turbine_positions, w, mesh, bcs, rho, depth, nu, cb, g, C_T, A_T, sigma)
+#    return turbine_power, velocity
