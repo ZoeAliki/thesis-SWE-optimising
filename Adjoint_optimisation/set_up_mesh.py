095d99c5296cd2048435bb439e61fb2c82f0cf62
ZoeAliki <zabreedveld@gmail.com>
2025-12-28 11:24:38 +0100

    Update notebooks and solver

--- Adjoint_optimisation/set_up_mesh.py
+++ Adjoint_optimisation/set_up_mesh.py
@@ -1,10 +1,17 @@
 import matplotlib.pyplot as plt
 import dolfin as dlf
+import dolfin_adjoint as adj
 
 def mesh_set_up(Lx, Ly, Nx, Ny, showplot):
     mesh_dolfin = dlf.RectangleMesh(dlf.Point(0.0, 0.0),
                                     dlf.Point(Lx, Ly),
                                     Nx, Ny)
+    # Wrap with fenics_adjoint Mesh wrapper so it can be recorded as a dependency
+    try:
+        mesh_dolfin = adj.Mesh(mesh_dolfin)
+    except Exception:
+        # If wrapping fails (old/new package versions), continue with unwrapped mesh
+        pass
 
     V_elem = dlf.VectorElement("CG", mesh_dolfin.ufl_cell(), 2)
     Q_elem = dlf.FiniteElement("CG", mesh_dolfin.ufl_cell(), 1)
