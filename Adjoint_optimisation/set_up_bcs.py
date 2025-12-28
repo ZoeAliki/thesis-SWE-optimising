337f5110b005c4de0192535fccd7771ce2b2d08b
ZoeAliki <zabreedveld@gmail.com>
2025-12-28 10:06:34 +0100

    Update notebooks and solver

--- Adjoint_optimisation/set_up_bcs.py
+++ Adjoint_optimisation/set_up_bcs.py
@@ -39,3 +39,34 @@ def setup_boundary_markers_and_bcs(mesh, W, Lx, Ly, U_inflow):
     print( "   - Walls  (ID=3): free‑slip (no Dirichlet BC)\n")
 
     return boundary_markers, bcs
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.patches import Circle
+
+def place_turbines_random2(Lx, Ly, n_turbines, min_spacing, D, type, seed=None, margin=None, max_attempts=20):
+    if seed is not None:
+        np.random.seed(seed)
+
+    if margin is None:
+        margin = max(min_spacing / 2, D)
+
+    positions = []
+    attempts = 0
+
+
+    while len(positions) < n_turbines and attempts < max_attempts:
+        x = np.random.uniform(margin, Lx - margin)
+        y = np.random.uniform(margin, Ly - margin)
+        new_pos = np.array([x, y])
+
+        if all(np.linalg.norm(new_pos - np.array(pos)) >= min_spacing for pos in positions):
+            positions.append(new_pos)
+
+        attempts += 1
+
+    if len(positions) < n_turbines:
+        raise RuntimeError("Failed to place all turbines with the given constraints.")
+
+    return np.array(positions)
